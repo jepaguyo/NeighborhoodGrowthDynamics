941c18f3211883d87a0220fe85b80300fbb31bdd
J.E. Paguyo <je.paguyo@gmail.com>
2020-04-28 18:17:05 -0700

    Added transformation function

--- NeighborhoodGrowth.py
+++ NeighborhoodGrowth.py
@@ -10,6 +10,7 @@ class Dynamics:
     def __init__(self, young_diagram, site_configuration):
         self.young_diagram = self.zero_set(young_diagram)
         self.site_configuration = self.normalize_site_configuration(site_configuration)
+        self.make_bounding_box()
 
     def normalize_site_configuration(self, site_configuration): 
         minimum_x_value = min(map(lambda t: t[0], site_configuration))
@@ -24,6 +25,54 @@ class Dynamics:
                 growth_sites.append((j,i))
         return growth_sites
 
+    def make_bounding_box(self):
+        maximum_x_value = max(map(lambda t: t[0], self.site_configuration))
+        maximum_y_value = max(map(lambda t: t[1], self.site_configuration))
+        bounding_box_width = 2 * maximum_x_value + 1
+        bounding_box_height = 2 * maximum_y_value + 1
+        self.bounding_box = []
+        for i in range(0,bounding_box_height + 1):
+            self.bounding_box.append([False]*(bounding_box_width+1))
+        for (i,j) in self.site_configuration:
+            self.bounding_box[j][i] = True
+
+    def row_count(self, row_index):
+        count = 0
+        for on in self.bounding_box[row_index]:
+            if on:
+                count += 1
+        return count
+
+    def col_count(self, col_index):
+        count = 0
+        for row in self.bounding_box:
+            if row[col_index]:
+                count += 1
+        return count 
+
+    def turn_on(self, i, j):
+        row_c = self.row_count(j)
+        col_c = self.col_count(i)
+        return (row_c, col_c) in self.young_diagram
+
+    def transform(self): 
+        updated_bounding_box = []
+        maximum_x_value = max(map(lambda t: t[0], self.site_configuration))
+        maximum_y_value = max(map(lambda t: t[1], self.site_configuration))
+        bounding_box_width = 2 * maximum_x_value + 1
+        bounding_box_height = 2 * maximum_y_value + 1
+        for i in range(0,bounding_box_height + 1):
+            updated_bounding_box.append([False]*(bounding_box_width + 1))
+        for j in range(0,bounding_box_height + 1):
+            for i in range(0,bounding_box_width + 1):
+                # print("x", (i,j))
+                # print(updated_bounding_box[j][i])
+                # print(self.bounding_box[j][i])
+                updated_bounding_box[j][i] = self.bounding_box[j][i] or not self.turn_on(i,j)
+                # updated_bounding_box[j][i] = self.bounding_box[j][i] or self.turn_on(i,j) 
+
+        self.bounding_box = updated_bounding_box
+
 
 def threshold_set(n):
     threshold_diagram = []
@@ -31,10 +80,16 @@ def threshold_set(n):
         threshold_diagram.append(n + 1 - i)
     return threshold_diagram
 
-zeroset = threshold_set(5)
+zeroset = threshold_set(4)
 # zeroset = [4,3,2,1]
-print (Dynamics(zeroset, [(2,5), (-3,1), (8, 5), (4,7)]).site_configuration)
-print (Dynamics(zeroset, [(2,5), (-3,1), (8, 5), (4,7)]).young_diagram)
+# print (Dynamics(zeroset, [(2,5), (-3,1), (8, 5), (4,7)]).site_configuration)
+test = Dynamics(zeroset, [(0,4),(0,3),(1,2),(2,1),(3,0),(4,0)])
+print(test.young_diagram)
+print(test.bounding_box)
+for i in range(0,6):   
+    test.transform()
+    print(test.bounding_box)
+
 
 
 # Young diagram by list
